2e8eb40cbfcf44f526434c5ff55cfe61f679149e
manab oz <staff@oz-edu.com>
2021-12-21 15:57:58 +0900

    first commit

--- shooting-game/GameScene.swift
+++ shooting-game/GameScene.swift
@@ -1,88 +1,75 @@
-//
-//  GameScene.swift
-//  shooting-game
-//
-//  Created by 辻大地 on 2021/12/21.
-//
-
 import SpriteKit
 import GameplayKit
-
+import CoreMotion // CMMotionManagerを利用とすると端末の傾きを取得できる
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let motionManager = CMMotionManager()
+    var accelaration: CGFloat = 0.0
     
+    var timer: Timer?
+
+    var earth: SKSpriteNode!
+    var spaceship: SKSpriteNode!
+
     override func didMove(to view: SKView) {
+        self.earth = SKSpriteNode(imageNamed: "earth")
+        self.earth.xScale = 1.5
+        self.earth.yScale = 0.3
+        self.earth.position = CGPoint(x: 0, y: -frame.height / 2)
+        self.earth.zPosition = -1.0
+        addChild(self.earth)
+
+        self.spaceship = SKSpriteNode(imageNamed: "spaceship")
+        self.spaceship.scale(to: CGSize(width: frame.width / 5, height: frame.width / 5))
+        self.spaceship.position = CGPoint(x: 0, y: self.earth.frame.maxY + 50)
+        addChild(self.spaceship)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        motionManager.accelerometerUpdateInterval = 0.2
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, _) in
+                guard let data = data else { return }
+                let a = data.acceleration
+                self.accelaration = CGFloat(a.x) * 0.75 + self.accelaration * 0.25
+            }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+                    self.addAsteroid()
+                })
         }
-    }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+
+        override func didSimulatePhysics() {
+            let nextPosition = self.spaceship.position.x + self.accelaration * 50
+            if nextPosition > frame.width / 2 - 30 { return }
+            if nextPosition < -frame.width / 2 + 30 { return }
+            self.spaceship.position.x = nextPosition
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        let missile = SKSpriteNode(imageNamed: "missile")
+        missile.position = CGPoint(x: self.spaceship.position.x, y: spaceship.position.y + 50)
+        addChild(missile)
+        
+        let moveToTop = SKAction.moveTo(y: frame.height + 10, duration: 0.3)
+        let remove    = SKAction.removeFromParent()
+        missile.run(SKAction.sequence([moveToTop, remove]))
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    func addAsteroid() {
+            let names = ["asteroid1", "asteroid2", "asteroid3"]
+            let index = Int(arc4random_uniform(UInt32(names.count)))
+            let name = names[index]
+            let asteroid = SKSpriteNode(imageNamed: name)
+            let random = CGFloat(arc4random_uniform(UINT32_MAX)) / CGFloat(UINT32_MAX)
+            let positionX = frame.width * (random - 0.5)
+            asteroid.position = CGPoint(x: positionX, y: frame.height / 2 + asteroid.frame.height)
+            asteroid.scale(to: CGSize(width: 70, height: 70))
+            addChild(asteroid)
+
+            let move = SKAction.moveTo(y: -frame.height / 2 - asteroid.frame.height, duration: 6.0)
+            let remove = SKAction.removeFromParent()
+            asteroid.run(SKAction.sequence([move, remove]))
+        }
+
 }
